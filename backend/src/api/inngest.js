ab502b7e1d21a7801cc46aea265ec37be9a84507
Amar-Lahlouh <amarlahlouh5@gmail.com>
2026-06-30 13:54:13 +0300

    fixinf importing

--- backend/src/api/inngest.js
+++ backend/src/api/inngest.js
@@ -1,5 +1,5 @@
 import { serve } from "inngest/express";
-import { inngest, functions } from "../src/config/inngest.js";
+import { inngest, functions } from "../config/inngest.js";
 
 export default serve({
   client: inngest,
